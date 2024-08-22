b9f083633f0a2006b91842cfd0ac5ed2f6d7d8a2
Syed Arshman <syedarshman809@gmail.com>
2024-08-22 15:59:39 +0500

    Add files via upload
    Signed-off-by: Syed Arshman <syedarshman809@gmail.com>

---
+++ Arshman/script.js
@@ -0,0 +1,23 @@
+var number1 = prompt("Enter your first number");
+var number2 = prompt("Enter your second number");
+var operater = prompt("Enter your operater");
+
+if(operater == "+"){
+    var result = number1 + number2
+    alert(result)
+}
+
+if(operater == "-"){
+    var result= number1 - number2
+    alert(result)
+}
+
+if(operater == "*"){
+    var result= number1 * number2
+    alert(result)
+}
+
+if(operater == "/"){
+    var result= number1 / number2
+    alert(result)
+}
